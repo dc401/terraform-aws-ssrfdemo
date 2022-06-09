eee054a8fc20854682c53e5c0a43d42254583601
SCIS Security <4706268+dc401@users.noreply.github.com>
2022-06-09 12:48:58 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,2 @@
-#outputs for terraform-aws-ssrf_instance module
-#no outputs
+#outputs for terraform-aws-ssrfdemo main module
+#No default outputs but you can add some of the ones from the other outputs.tf files since main.tf calls the other 2 modules as dependencies.
